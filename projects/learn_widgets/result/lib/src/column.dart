2858e4a560cf504aa0bf93455f9c106cc2bf5375
Payam Zahedi <pzahedi1995@gmail.com>
2022-08-30 19:51:11 +0430

    add row and column widget

--- projects/learn_widgets/result/lib/src/column.dart
+++ projects/learn_widgets/result/lib/src/column.dart
@@ -5,6 +5,56 @@ class LearnColumn extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: Colors.black26,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Container(
+            width: 100,
+            height: 100,
+            alignment: Alignment.center,
+            color: Colors.red,
+            child: Text(
+              "I'm a Container",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Container(
+            width: 200,
+            height: 200,
+            alignment: Alignment.center,
+            color: Colors.green,
+            child: Text(
+              "I'm a Container",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Container(
+            width: 100,
+            height: 100,
+            alignment: Alignment.center,
+            color: Colors.blue,
+            child: Text(
+              "I'm a Container",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
