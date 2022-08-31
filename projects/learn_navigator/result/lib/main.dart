b7bf0d2904c7fd36b36599814680def653398642
Payam Zahedi <pzahedi1995@gmail.com>
2022-08-31 23:34:07 +0430

    add learn navigator app

---
+++ projects/learn_navigator/result/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      title: 'Navigation Basics',
+      home: FirstRoute(),
+    ),
+  );
+}
+
+class FirstRoute extends StatelessWidget {
+  const FirstRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First Route'),
+      ),
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Route'),
+      ),
+    );
+  }
+}
