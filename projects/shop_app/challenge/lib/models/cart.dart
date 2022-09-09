2390090cf21dd46f7db39e18d23b40ad1ad12d10
Payam Zahedi <pzahedi1995@gmail.com>
2022-09-09 13:35:45 +0430

    add shop app challenge

---
+++ projects/shop_app/challenge/lib/models/cart.dart
@@ -0,0 +1,7 @@
+
+import 'package:flutter/foundation.dart';
+import 'package:result/models/catalog.dart';
+
+// TODO: complete this class 
+class CartModel extends ChangeNotifier {
+}
---
+++ projects/shop_app/challenge/lib/screens/login.dart
@@ -0,0 +1,53 @@
+// Copyright 2020 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:result/screens/catalog.dart';
+
+class MyLogin extends StatelessWidget {
+  const MyLogin({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(80.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Welcome',
+                style: Theme.of(context).textTheme.displayLarge,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Username',
+                ),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Password',
+                ),
+                obscureText: true,
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // TODO: add navigation
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.yellow,
+                ),
+                child: const Text('ENTER'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
