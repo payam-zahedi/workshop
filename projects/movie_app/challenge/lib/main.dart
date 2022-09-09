681bed69a5355dfc6a52cc01231cae58845dde70
Payam Zahedi <pzahedi1995@gmail.com>
2022-09-09 22:20:32 +0430

    add movie app challenge

--- projects/movie_app/challenge/lib/main.dart
+++ projects/movie_app/challenge/lib/main.dart
@@ -22,33 +22,6 @@ class _App extends State<App> {
   List<Movie> _movies = [];
 
   @override
-  void initState() {
-    super.initState();
-    _populateAllMovies();
-  }
-
-  void _populateAllMovies() async {
-    final movies = await _fetchAllMovies();
-    setState(() {
-      _movies = movies;
-    });
-  }
-
-  Future<List<Movie>> _fetchAllMovies() async {
-    final response = await http.get(
-      Uri.parse(apiUrl),
-    );
-
-    if (response.statusCode == 200) {
-      final result = jsonDecode(response.body);
-      Iterable list = result["Search"];
-      return list.map((movie) => Movie.fromJson(movie)).toList();
-    } else {
-      throw Exception("Failed to load movies!");
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Movies App",
---
+++ projects/movie_app/challenge/lib/widgets/movies_list.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:result/models/movie.dart';
+import 'package:result/widgets/movie_detail.dart';
+
+class MoviesListWidget extends StatelessWidget {
+  const MoviesListWidget({
+    Key? key,
+    required this.movies,
+  }) : super(key: key);
+
+  final List<Movie> movies;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Movies')),
+      body: ListView.builder(
+        itemCount: movies.length,
+        itemBuilder: (context, index) {
+          final movie = movies[index];
+
+          return ListTile(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => MovieDetail(movie: movie),
+                ),
+              );
+            },
+            title: Row(
+              children: [
+                SizedBox(
+                  width: 100,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.network('Image poster'),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Title'),
+                        Text('Year'),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
